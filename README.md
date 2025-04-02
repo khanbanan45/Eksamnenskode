afa19983c716d0ae6e7355749d77aac1e2f8e7de
khanbanan45 <NEXT16306@edu.nextkbh.dk>
2025-04-02 08:37:22 +0200

    20/3 planeter

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Eksamnenskode
 
-hej skibidi
-r4uruir
-2342342
-dfsfs
+
--- sketch.js
+++ sketch.js
@@ -1,9 +1,39 @@
-function setup() 
-{
-	createCanvas(400, 400);
-}
+let G = 6.67430e-11; // Tyngdekonstanten, en universal konstant, som beskriver tyngdekraftens styrke.
+let sol; // Variabel for at gemme solens data
+let jord; // Variabel til for at gemme Jordens data
+let jordKredsløb = []; // Dette er et array, der skal gemme jordens kredsløb, "jordkredsløb"
+const maksKredsløbPunkter = 30000; // Maks antal punkter i Jordens bane
+const AU = 149.6e9; // 1 Astronomisk Enhed = afstand Jorden-Solen (meter)
+const skala = 250 / AU; // Omregningsfaktor fra meter til pixels
 
-function draw()
-{
-ihig
-}
+
+function setup() {
+  createCanvas(1000, 800); // Laver et lærred 1000x800 pixels
+  
+  // Laver en hastigheds-regulator (slider)
+  slider = createSlider(1, maxTidsskala, 1);
+  slider.position(20, 150); // Placering på skærmen
+  slider.style('width', '200px'); // Bredde
+  
+  // Solens egenskaber
+  sol = {
+    x: width/2, // X-position (midt på skærmen)
+    y: height/2, // Y-position (midt på skærmen)
+    diameter: 40, // Størrelse i pixels
+    masse: 1.989e30, // Vægt i kg (meget stor!)
+    vx: 0, // Hastighed i x-retning (0 = står stille)
+    vy: 0, // Hastighed i y-retning
+    farve: [255, 204, 0] // Gul farve (RGB)
+  };
+  
+  // Jordens egenskaber
+  jord = {
+    x: width/2 + AU * skala, // Startposition (1 AU fra Solen)
+    y: height/2,
+    diameter: 12, // Størrelse i pixels
+    masse: 5.972e24, // Vægt i kg
+    vx: 0, // Start hastighed i x-retning
+    vy: -29.78e3 * skala, // Hastighed i y-retning (29.78 km/s)
+    farve: [0, 100, 255] // Blå farve
+  };
+};
